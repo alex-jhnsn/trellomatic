788d653e50fadb7471cb6fd0bf14e5f9b63e163a
Alex Johnson <alextheviking@hotmail.co.uk>
2018-10-10 17:12:19 +0100

    Can now send emails

--- Email.js
+++ Email.js
@@ -1,12 +1,21 @@
-function sendMessage(userId, email, callback) {
-    // Using the js-base64 library for encoding:
-    // https://www.npmjs.com/package/js-base64
-    var base64EncodedEmail = Base64.encodeURI(email);
-    var request = gapi.client.gmail.users.messages.send({
-      'userId': userId,
-      'resource': {
-        'raw': base64EncodedEmail
-      }
-    });
-    request.execute(callback);
+var email 	= require("emailjs");
+var server 	= email.server.connect({
+   user:    process.env.EMAIL_ADDRESS, 
+   password:process.env.EMAIL_PASSWORD, 
+   host:    "smtp.gmail.com", 
+   ssl:     true
+});
+ 
+
+
+module.exports = {
+  Send: function(boardName, boardJson) {
+    server.send({
+      text:    boardJson, 
+      from:    process.env.EMAIL_ADDRESS, 
+      to:      process.env.RECIPIENTS,
+      subject: boardName
+    }, function(err, message) {  });
+  }
 }
+
--- Index.js
+++ Index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 var BoardReader = require("./BoardReader");
+var Email = require("./Email");
 
 //To Be Imported From Siri Shortcut
 const boardId = process.env.TRELLO_BOARD_ID;
@@ -9,14 +10,18 @@ const trello_api_token = process.env.TRELLO_API_TOKEN;
 const auth = "key=" + trello_api_key + "&token=" + trello_api_token;
 
 //POST emails from SIRI shortcut?
-var emails = ["alextheviking@hotmail.co.uk"];
+
+var recipients = process.env.RECIPIENTS;
 
 // var res = curl lambda url
 // var url = new URL(res.url)
 // var boardId = url.searchParams.get("boardId");
 
+var boardName = BoardReader.GetBoardTitle(boardId, auth);
 var boardJson = BoardReader.ReadBoard(boardId, auth);
 
-boardJson.then(value => {
-    console.log(value);
+var test = Promise.all([boardName, boardJson]);
+
+test.then(value => {
+    Email.Send(value[0], value[1]);
 })
